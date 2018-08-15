48458cc3be70c673cef5d1c78960b2e576019be5
O M E N <O M E N@LAPTOP-PJ10TE7S>
2018-08-15 10:04:07 +0700

    Coba method format

--- src/jobsheet11.java
+++ src/jobsheet11.java
@@ -68,6 +68,9 @@ public static void main(String[] args)
         for(int i=0;i<arrTr.length;i++)
             System.out.println(arrTr[i]);
         
-        
+        double d = 1545454000;
+                System.out.println(String.format("Uang saya %,.4f", d));
+                
+                System.out.println(String.format(Locale.forLanguageTag("in-ID"), "Uang saya Rp %,.2f", d));
 }
 }

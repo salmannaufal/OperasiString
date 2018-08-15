5ddd7c19c6538bce6dc974884f8195d0187d8daa
O M E N <O M E N@LAPTOP-PJ10TE7S>
2018-08-15 10:00:31 +0700

    Coba method compareTo & compareToIgnoreCase

--- src/jobsheet11.java
+++ src/jobsheet11.java
@@ -34,6 +34,10 @@ public static void main(String[] args)
         System.out.println("perbandingan isi x dengan z (Case Sensitive): " + x.compareTo(z));
         System.out.println("perbandingan isi x dengan r (Case Sensitive): " + x.compareToIgnoreCase(r));
         
+        String s = "operasI";
+        System.out.println("isi variable s : "+ s);
+        System.out.println("perbandingan isi r dengan s (Case Sensitive) : " + r.compareTo(s));
+        
         
 }
 }

1d46ea71d297a6f1f0cc08dd945427adaaa87d10
O M E N <O M E N@LAPTOP-PJ10TE7S>
2018-08-15 09:57:31 +0700

    Coba method equals & equalsIgnoreCase

--- src/jobsheet11.java
+++ src/jobsheet11.java
@@ -19,6 +19,21 @@ public static void main(String[] args)
         System.out.println("isi variabel y : "+ y);
         System.out.println("y adalah kosong : " + y.isEmpty());
         
+        System.out.println("isi x sama dengan y : " + x.equals(y));
+        
+        String z = "Opersi";
+        System.out.println("isi variable z : "+ z);
+        System.out.println("isi x sama dengan z (Case Sensitive): " + x.equals(z));
+        
+        String r = "operasi";
+        System.out.println("isi cariable r : "+ r);
+        System.out.println("isi variable s sama dengan r (Case Sensitive): " + x.equals(r));
+        System.out.println("isi x sama dengan r (Case Sensitive) : " + x.equalsIgnoreCase(r));
+        
+        System.out.println("perbandingan isi x dengan y : " + x.compareTo(y));
+        System.out.println("perbandingan isi x dengan z (Case Sensitive): " + x.compareTo(z));
+        System.out.println("perbandingan isi x dengan r (Case Sensitive): " + x.compareToIgnoreCase(r));
+        
         
 }
 }

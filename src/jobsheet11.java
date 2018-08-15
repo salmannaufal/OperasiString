5cd4b983acf898e7e380e0e3ba22a261c15c16e0
O M E N <O M E N@LAPTOP-PJ10TE7S>
2018-08-15 09:50:16 +0700

    Initial Commit

---
+++ src/jobsheet11.java
@@ -0,0 +1,14 @@
+
+import java.util.Locale;
+
+
+public class jobsheet11 {
+public static void main(String[] args)
+{
+    
+        String identitas = "Salman Naufal Haq / XRPL2 / 31";
+        System.out.println("Identitas : "+ identitas);
+   
+        
+}
+}

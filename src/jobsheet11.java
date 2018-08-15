67020b1bdc89a5d043b8fae2017fb761705b033a
O M E N <O M E N@LAPTOP-PJ10TE7S>
2018-08-15 09:55:07 +0700

    Coba method length & isEmpty

--- src/jobsheet11.java
+++ src/jobsheet11.java
@@ -9,6 +9,16 @@ public static void main(String[] args)
         String identitas = "Salman Naufal Haq / XRPL2 / 31";
         System.out.println("Identitas : "+ identitas);
    
+        String x = "Operasi";
+        System.out.println("isi variabel  x : " + x);
+        System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variabel y : "+ y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
+        
         
 }
 }
